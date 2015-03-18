drtshock/HCFactions

commit 6eb854608c1d7d7f368bdfa090527c1b2dfb9007

Update config docs for sidebars
Also make better default settings.
Fixes https://github.com/drtshock/HCFProblems/issues/20
Also fixes #16, but we did not finish adding the first part of the
issue.

src/main/resources/config.yml
@@ -93,6 +93,7 @@ scoreboard:
 # FInfo scoreboard is displayed when a player walks into a new Faction's territory.
 # Scoreboard disappears after <expiration> seconds.
 # Things to be replaced in this:
+# {name} - viewer name. {balance} - viewer balance. {faction} faction tag (FYI: title of board is already faction tag)
 # {dtr} - Factions current DTR. {maxdtr} - Max DTR that faction can have
 # {land} - total claimed land (in chunks). {maxland} - how much land a faction can claim
 # {members} - total members. {online} - online members. {leader} - faction's leader.
@@ -104,17 +105,20 @@ scoreboard:
   finfo:
     - "&3Members"
     - "{online}/{members}"
-    - "&4Leader"
-    - "{leader}"
-    - "&bTerritory"
-    - "{chunks}"
+    - "&4DTR"
+    - "{dtr}/{maxdtr}"
+    - "&9Raidable: {raidable}"
+    - "&bLand"
+    - "{land}/{maxland}"
 
 # Default board that will always show up if a player wants it to.
 # This can show any arbitrary text or you can use a lot of variables to replace things.
-# Replace {name} - player's name. {faction} - player's faction title, factionless if none.
-# {totalOnline} - total players on the server. {balance} - player's balance.
-# {maxPower} - player's max power.
-# {powerBoost} - player's powerboost.
+# {name} - viewer name. {balance} - viewer balance. {faction} - faction tag
+# {dtr} - Factions current DTR. {maxdtr} - Max DTR that faction can have
+# {land} - total claimed land (in chunks). {maxland} - how much land a faction can claim
+# {members} - total members. {online} - online members. {leader} - faction's leader.
+# {raidable} - true if the faction can be claimed over, otherwise false.
+# {warps} - the number of warps that a faction has set.
 
   default-enabled: false # Default to false to keep original functionality.
   default-title: "i love drt" # Can use any of the values from above but this won't update once it's set (so don't set {balance}).
@@ -127,6 +131,8 @@ scoreboard:
   default:
     - "&6Your Faction"
     - "{faction}"
+    - "&4Faction DTR"
+    - "{dtr}/{maxdtr}"
     - "&aBalance"
     - "${balance}"
 

commit 25de493aaf551a29b8c035a6cb0706323812ea10

Add totalOnline variable
https://github.com/drtshock/HCFactions/commit/6eb854608c1d7d7f368bdfa090527c1b2dfb9007
broke totalOnline variable. This commit adds it back in.

Fixes https://github.com/drtshock/HCFProblems/issues/21

src/main/java/com/massivecraft/factions/scoreboards/FSidebarProvider.java
@@ -5,6 +5,7 @@ import com.massivecraft.factions.Faction;
 import com.massivecraft.factions.integration.Econ;
 import com.massivecraft.factions.zcore.util.TL;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import java.text.DecimalFormat;
@@ -30,7 +31,7 @@ public abstract class FSidebarProvider {
         s = s.replace("{online}", String.valueOf(faction.getOnlinePlayers().size())).replace("{members}", String.valueOf(faction.getFPlayers().size()));
         s = s.replace("{leader}", String.valueOf(leader)).replace("{land}", String.valueOf(faction.getLand())).replace("{maxland}", String.valueOf(faction.getMaxLand()));
         s = s.replace("{raidable}", raidable ? TL.RAIDABLE_TRUE.toString() : TL.RAIDABLE_FALSE.toString());
-        s = s.replace("{warps}", String.valueOf(faction.getWarps().size()));
+        s = s.replace("{warps}", String.valueOf(faction.getWarps().size())).replace("{totalOnline}", String.valueOf(Bukkit.getServer().getOnlinePlayers().size()));
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 }

src/main/resources/config.yml
@@ -98,7 +98,7 @@ scoreboard:
 # {land} - total claimed land (in chunks). {maxland} - how much land a faction can claim
 # {members} - total members. {online} - online members. {leader} - faction's leader.
 # {raidable} - true if the faction can be claimed over, otherwise false.
-# {warps} - the number of warps that a faction has set.
+# {warps} - the number of warps that a faction has set. {totalOnline} - # of online players
 # The title of the scoreboard will be the Faction's tag and colored according to the relation with the player's Faction.
 # Commenting this section out will cause the info to appear in chat as the plugin originally did.
   finfo-enabled: false # Default to false to keep original functionality.
@@ -118,7 +118,7 @@ scoreboard:
 # {land} - total claimed land (in chunks). {maxland} - how much land a faction can claim
 # {members} - total members. {online} - online members. {leader} - faction's leader.
 # {raidable} - true if the faction can be claimed over, otherwise false.
-# {warps} - the number of warps that a faction has set.
+# {warps} - the number of warps that a faction has set. {totalOnline} - # of online players
 
   default-enabled: false # Default to false to keep original functionality.
   default-title: "i love drt" # Can use any of the values from above but this won't update once it's set (so don't set {balance}).