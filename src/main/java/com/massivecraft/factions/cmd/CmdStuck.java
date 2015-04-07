a3809ddf37880163c74681af9eb3d6ca8dca4613
Nick Porillo <nsporillo@gmail.com>
2015-04-07 15:32:27 -0400

    Add command cost and essentials teleporting
    Also polished off the logic, there was potential issues that could arise
    that appeared in more in depth testing. They are fixed with this commit.

--- src/main/java/com/massivecraft/factions/cmd/CmdStuck.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdStuck.java
@@ -1,7 +1,9 @@
 package com.massivecraft.factions.cmd;
 
+import com.massivecraft.factions.Conf;
 import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.P;
+import com.massivecraft.factions.integration.Essentials;
 import com.massivecraft.factions.struct.Permission;
 import com.massivecraft.factions.zcore.util.TL;
 import org.apache.commons.lang.time.DurationFormatUtils;
@@ -12,10 +14,12 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.BitSet;
 import java.util.List;
+import java.util.Random;
 
 public class CmdStuck extends FCommand {
+
+    private final static Random random = new Random();
 
     public CmdStuck() {
         super();
@@ -35,15 +39,21 @@ public class CmdStuck extends FCommand {
     @Override
     public void perform() {
         final Player player = fme.getPlayer();
+        final Location sentAt = player.getLocation();
         final FLocation chunk = fme.getLastStoodAt();
         final long delay = P.p.getConfig().getLong("hcf.fstuck.delay", 60);
+        final int radius = P.p.getConfig().getInt("hcf.fstuck.radius", 10);
 
         if (P.p.getStuckMap().containsKey(player.getUniqueId())) {
             long wait = P.p.getTimers().get(player.getUniqueId()) - System.currentTimeMillis();
             String time = DurationFormatUtils.formatDuration(wait, TL.COMMAND_STUCK_TIMEFORMAT.toString(), true);
             msg(TL.COMMAND_STUCK_EXISTS, time);
         } else {
-            final List<Integer> blackList = P.p.getConfig().getIntegerList("hcf.fstuck.black-list");
+
+            // if economy is enabled, they're not on the bypass list, and this command has a cost set, make 'em pay
+            if (!payForCommand(Conf.econCostStuck, TL.COMMAND_STUCK_TOSTUCK.format(fme.getName()), TL.COMMAND_STUCK_FORSTUCK.format(fme.getName()))) {
+                return;
+            }
 
             final int id = Bukkit.getScheduler().runTaskLater(P.p, new BukkitRunnable() {
 
@@ -52,32 +62,56 @@ public class CmdStuck extends FCommand {
                     if (!P.p.getStuckMap().containsKey(player.getUniqueId())) {
                         return;
                     }
+
+                    // check for world difference or radius exceeding
                     World world = chunk.getWorld();
-                    int cx = (int) chunk.getX() << 4;
-                    int cz = (int) chunk.getZ() << 4;
-
-                    // Max iterations worse case: 16x16x4 (1024 times)
-                    x : for (int x = cx; x < cx + 16; x++) {
-                        z : for (int z = cz; z < cz + 16; z++) {
-                            Block block = world.getHighestBlockAt(x, z);
-                            Location loc = block.getLocation();
-
-                            // make sure blocks under and above are okay
-                            for (int y = loc.getBlockY() - 1; y < loc.getBlockY() + 3; y++) {
-                                Block check = world.getBlockAt(x, y, z);
-                                if (blackList.contains(check.getTypeId())) {
-                                    continue z; // skip this coordinate
-                                }
-                            }
-                            P.p.debug("Teleported [" + fme.getName() + "] to x=" + loc.getBlockX() + ", y=" + loc.getBlockY() + ", z=" + loc.getBlockZ());
-                            player.teleport(loc);
-                            msg(TL.COMMAND_STUCK_TELEPORT, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-                            P.p.getTimers().remove(player.getUniqueId());
-                            P.p.getStuckMap().remove(player.getUniqueId());
-                            return;
-                        }
+                    if (world.getUID() != player.getWorld().getUID() || sentAt.distance(player.getLocation()) > radius) {
+                        msg(TL.COMMAND_STUCK_OUTSIDE.format(radius));
+                        P.p.getTimers().remove(player.getUniqueId());
+                        P.p.getStuckMap().remove(player.getUniqueId());
+                        return;
                     }
-                    msg(TL.COMMAND_STUCK_NOTELEPORT.toString());
+
+                    // pseudo random coord to get us out of most traps, +- 8 on the x and z
+                    Location location = sentAt.add(random.nextInt(16) - 8, 1, random.nextInt(16) - 8);
+
+                    // essentials safe teleport will take care of us :)
+                    if (Essentials.safeTeleport(player, location)) {
+                        Location loc = player.getLocation(); // players new location
+                        msg(TL.COMMAND_STUCK_TELEPORT, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+                        P.p.getTimers().remove(player.getUniqueId());
+                        P.p.getStuckMap().remove(player.getUniqueId());
+                    } else {
+                        List<Integer> blackList = P.p.getConfig().getIntegerList("hcf.fstuck.black-list");
+                        int cx = FLocation.chunkToBlock((int) chunk.getX());
+                        int cz = FLocation.chunkToBlock((int) chunk.getZ());
+
+                        for (int x = cx; x < cx + 16; x++) {
+                            z:
+                            for (int z = cz; z < cz + 16; z++) {
+                                Block block = world.getHighestBlockAt(x, z);
+                                Location loc = block.getLocation();
+
+                                // make sure blocks under and above are okay
+                                for (int y = loc.getBlockY() - 1; y < loc.getBlockY() + 3; y++) {
+                                    Block check = world.getBlockAt(x, y, z);
+                                    if (blackList.contains(check.getTypeId())) {
+                                        continue z; // skip this coordinate
+                                    }
+                                }
+
+                                player.teleport(loc);
+                                msg(TL.COMMAND_STUCK_TELEPORT, loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+                                P.p.getTimers().remove(player.getUniqueId());
+                                P.p.getStuckMap().remove(player.getUniqueId());
+                                return;
+                            }
+                        }
+                        P.p.debug("Failed to teleport [" + fme.getName() + "] safely");
+                        msg(TL.COMMAND_STUCK_NOTELEPORT.toString());
+                        P.p.getTimers().remove(player.getUniqueId());
+                        P.p.getStuckMap().remove(player.getUniqueId());
+                    }
                 }
             }, delay * 20).getTaskId();
 
